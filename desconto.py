57aa8071ac80d1e845a3f4eb062809ff6b50a01b
Rodrigo <rodrigodev2021@gmail.com>
2024-07-15 15:00:16 -0300

    ex012

---
+++ desconto.py
@@ -0,0 +1,3 @@
+preco = float(input('Qual é o preço do produto? '))
+novo_preco = preco - preco * 0.05
+print(f'O produto que custava R${preco:.2f}, com 5% de desconto vai custar R${novo_preco:.2f}')
