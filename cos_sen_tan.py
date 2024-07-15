57b93ea771919f1fa69a993a2787801061d38896
Rodrigo <rodrigodev2021@gmail.com>
2024-07-15 15:26:30 -0300

    ex018

---
+++ cos_sen_tan.py
@@ -0,0 +1,8 @@
+import math
+
+angulo = math.radians(float(input('Angulo qualquer: ')))
+
+print('Cosseno: ', math.cos(angulo))
+print('Seno: ', math.sin(angulo))
+print('Tangente: ', math.tan(angulo))
+    
