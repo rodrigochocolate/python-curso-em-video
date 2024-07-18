4b14547f754e9faf979f9af574999a78602bb704
Rodrigo <rodrigodev2021@gmail.com>
2024-07-18 15:04:17 -0300

    ex038

---
+++ comparando_numeros.py
@@ -0,0 +1,8 @@
+n1 = float(input('Primeiro número: '))
+n2 = float(input('Segundo número: '))
+if n1 > n2:
+    print('O primeiro número é o maior!')
+elif n1 < n2:
+    print('O segundo número é o maior!')
+else:
+    print('Os dois números são iguais!')
