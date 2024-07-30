eca780006d1e1d76bbf83298fc20842bec6d0021
Rodrigo <rodrigodev2021@gmail.com>
2024-07-30 13:47:52 -0300

    changed ex078

--- maior_menor_lista.py
+++ maior_menor_lista.py
@@ -1,14 +1,23 @@
 lista = []
+posMaior = []
+posMenor = []
 for c in range(5):
-    n = int(input('Digite o número: '))
+    n = int(input(f'Digite o número para a posição {c+1}: '))
     lista.append(n)
     if c == 0:
         maior = menor = n
     if n > maior:
         maior = n
+        
     elif n < menor:
         menor = n       
         
 print(lista)
-a = lista.rindex(maior)
-print(a)
+print(f'Maior foi {maior} nas posições ', end='')
+for pos1, x in enumerate(lista):
+    if x == maior:
+        print(pos1+1, end='... ')
+print(f'\nMenor foi {menor} nas posições ', end='')
+for pos2, y in enumerate(lista):
+    if y == menor:
+        print(pos2+1, end='... ')
