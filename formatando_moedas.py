a2973383ca5f9caf5cdd620cb1c7943a9a9807bd
Rodrigo <rodrigodev2021@gmail.com>
2024-08-15 16:11:52 -0300

    ex109

--- formatando_moedas.py
+++ formatando_moedas.py
@@ -1,6 +1,6 @@
 from funcoes_moeda import *
 
 preco = float(input('Digite um preço: R$'))
-print(f'A metade de {formatarDinheiro(preco, 'r$')} é igual a {formatarDinheiro(metade(preco), 'r$')}')
-print(f'A dobro de {formatarDinheiro(preco, 'r$')} é igual a {formatarDinheiro(dobro(preco), 'r$')}')
-print(f'Aumentando 10% de R${formatarDinheiro(preco, 'r$')}, temos {formatarDinheiro(aumentar(preco, 10))}')
+print(f'A metade de {formatarDinheiro(preco, 'r$')} é igual a {metade(preco, True)}')
+print(f'A dobro de {formatarDinheiro(preco, 'r$')} é igual a {dobro(preco, True)}')
+print(f'Aumentando 10% de R${formatarDinheiro(preco, 'r$')}, temos {aumentar(preco, 10, True)}')
--- funcoes_moeda.py
+++ funcoes_moeda.py
@@ -1,11 +1,11 @@
-def metade(num: float):
-    return num / 2
+def metade(num: float, formatar=False):
+    return formatarDinheiro(num / 2) if formatar else num / 2
 
-def dobro(num: float):
-    return num * 2
+def dobro(num: float, formatar=False):
+    return formatarDinheiro(num * 2) if formatar else num * 2
 
-def aumentar(num: float, porcentagem: float):
-    return num + num * porcentagem/100
+def aumentar(num: float, porcentagem: float, formatar=False):
+    return formatarDinheiro(num + num * porcentagem/100) if formatar else num + num * porcentagem/100
 
 def formatarDinheiro(dinheiro: float, moeda: str='R$'):
     moedaFormatada = moeda.upper() + str(round(dinheiro, 2)).replace('.', ',')
